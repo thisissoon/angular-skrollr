014e17dab33b9d6f1ceadfeb4349050eae5e1f27
James Warren <mail@jamesjwarren.com>
2015-02-02 16:12:12 +0000

    remove loading from SkrollrService

--- app/js/SkrollrService.js
+++ app/js/SkrollrService.js
@@ -1,28 +1,25 @@
 "use strict";
 /**
- * Load and initialise skrollrjs, based on:
- * {@link http://stackoverflow.com/questions/23715337/integrating-skrollr-w-angularjs-single-page-app}
+ * Initialise skrollrjs
  * @author SOON_
  * @module sn.skrollr
  * @class  snSkrollrService
  */
 angular.module("sn.skrollr").service("snSkrollrService", [
-    "$document",
     "$q",
     "$rootScope",
     "$window",
     /**
      * @constructor
-     * @param   {Object}  $document  angular wrapper for document
      * @param   {Service} $q         promise service
      * @param   {Object}  $rootScope data on rootScope
      * @param   {Object}  $window    angular wrapper for window object
      */
-    function($document, $q, $rootScope, $window){
+    function($q, $rootScope, $window){
 
         var defer = $q.defer();
 
-        function onScriptLoad() {
+        var onScriptLoad = function onScriptLoad() {
             // Load skrollr.js in the browser
             $rootScope.$apply(function() {
                 var s = $window.skrollr.init({
@@ -34,26 +31,9 @@ angular.module("sn.skrollr").service("snSkrollrService", [
             });
         }
 
-        /**
-         * Create a script tag with skrollr as the source
-         * and call our onScriptLoad callback when it
-         * has been loaded
-         */
-        var scriptTag = $document[0].createElement("script");
-        scriptTag.type = "text/javascript";
-        scriptTag.async = true;
-        scriptTag.src = "components/skrollr/dist/skrollr.min.js";
-
-        scriptTag.onreadystatechange = function () {
-            if (this.readyState === "complete") {
-                onScriptLoad();
-            }
-        };
-
-        scriptTag.onload = onScriptLoad;
-
-        var s = $document[0].getElementsByTagName("body")[0];
-        s.appendChild(scriptTag);
+        angular.element($window).bind('load', function() {
+            onScriptLoad();
+        });
 
         return {
             skrollr: function() { return defer.promise; }
