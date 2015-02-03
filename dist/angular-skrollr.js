76cf91f45c68f33bae84d0317491f82f4bcbc131
Edward Opare-Aryee <ed@thisissoon.com>
2015-02-03 16:06:18 +0000

    0.0.1

--- dist/angular-skrollr.js
+++ dist/angular-skrollr.js
@@ -1,109 +1,64 @@
 "use strict";
 /**
- * @module   sn.example
- * @main     sn.example
- * @author   SOON_
- * @requires ngRoute {@link https://docs.angularjs.org/api/ngRoute}
- */
-angular.module("sn.example", ["ngRoute"]);
-;"use strict";
-/**
- * Configuration for sn.example dependencies are set here.
- * @module sn.example
+ * Wrap skrollr.js
  * @author SOON_
+ * @module sn.skrollr
  */
-angular.module("sn.example").config([
-    "$routeProvider",
-    "$locationProvider",
-    /**
-     * @constructor
-     * @param {Service} $routeProvider
-     * @param {Service} $locationProvider
-     */
-    function ($routeProvider, $locationProvider) {
+angular.module("sn.skrollr", [])
 
-        $locationProvider.html5Mode(true).hashPrefix = "!";
-
-        $routeProvider
-            .when("/", {
-                templateUrl: "partials/search.html",
-                controller: "SearchCtrl"
-            })
-            .when("/results", {
-                templateUrl: "partials/results.html",
-                controller: "ResultsCtrl"
-            })
-            .otherwise({
-                redirectTo: "/"
-            });
-
-    }
-]);
-;"use strict";
 /**
- * Controller to perform search in sn.example.
- * @class  SearchCtrl
- * @module sn.example
- * @author SOON_
+ * Provider to configuring skrollr
+ * @example
+ *      snSkrollrProvider.init({ smoothScrolling: true });
+ *      snSkrollr.init();
  */
-angular.module("sn.example").controller("SearchCtrl", [
-    "$scope",
-    "$rootScope",
-    "$http",
-    "$location",
-    /**
-     * @constructor
-     * @param {Object}  $scope
-     * @param {Service} $rootScope
-     * @param {Service} $http
-     * @param {Service} $location
-     */
-    function ($scope, $rootScope, $http, $location) {
+.provider("snSkrollr", function snSkrollrProvider() {
 
+    var _this = this;
+
+    this.config = {};
+
+    this.$get = [
+        "$window",
+        "$document",
+        "$rootScope",
         /**
-         * Search locations based on val
-         * @method getLocation
-         * @param  {String} val location to query
+         * @constructor
+         * @param   {Object}  $window    angular wrapper for window
+         * @param   {Object}  $rootScope angular root application scope
          */
-        $scope.getLocation = function getLocation(val){
-            $http.get("http://maps.googleapis.com/maps/api/geocode/json", {
-                params: {
-                    address: val,
-                    sensor: false
+        function($window, $document, $rootScope) {
+
+            return {
+                init: function() {
+
+                    $document.ready(function () {
+                        $rootScope.$apply(function() {
+                            var s = $window.skrollr.init(_this.config);
+                        });
+                    });
+
                 }
-            }).then(function (response){
-                $rootScope.results = response.data.results;
-                $location.path("/results");
-            });
-        };
+            };
+        }
+    ];
+})
 
-    }
-
-]);
-;"use strict";
 /**
- * Controller to display results of search in sn.example.
- * @class  ResultsCtrl
- * @module sn.example
- * @author SOON_
+ * Refresh skrollrjs on element init
+ * @class  snSkrollr
  */
-angular.module("sn.example").controller("ResultsCtrl", [
-    "$scope",
-    "$rootScope",
+.directive("snSkrollr", [
+    "$window",
     /**
      * @constructor
-     * @param {Object}  $scope
-     * @param {Service} $rootScope
      */
-    function ($scope, $rootScope) {
-
-        /**
-         * The result from the search query
-         * @property results
-         * @type     {Array}
-         */
-        $scope.results = $rootScope.results;
-
+    function ($window){
+        return {
+            restrict: "AE",
+            link: function($scope, $element) {
+                $window.skrollr.refresh();
+            }
+        };
     }
-
 ]);
